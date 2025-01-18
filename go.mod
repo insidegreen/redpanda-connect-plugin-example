4b9d10227930983b8cf18e0d5a3d9a4e0b02e4ae
Tom Hieß <tom.hiess@uniserv.com>
2025-01-18 13:05:01 +0100

    first benthos batch sync response impl

--- go.mod
+++ go.mod
@@ -227,6 +227,7 @@ require (
 	github.com/neo4j/neo4j-go-driver/v5 v5.24.0 // indirect
 	github.com/nsf/jsondiff v0.0.0-20210926074059-1e845ec5d249 // indirect
 	github.com/nsqio/go-nsq v1.1.0 // indirect
+	github.com/numaproj/numaflow-go v0.9.0
 	github.com/oapi-codegen/runtime v1.1.1 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
@@ -310,22 +311,22 @@ require (
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
 	golang.org/x/mod v0.20.0 // indirect
-	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/oauth2 v0.22.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/term v0.23.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/term v0.24.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	google.golang.org/api v0.188.0 // indirect
 	google.golang.org/genproto v0.0.0-20240708141625-4ad9e859172b // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240709173604-40e1e62336c5 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240827150818-7e3bb234dfed // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/grpc v1.66.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
--- input/numa-reduce/numa-reduce.go
+++ input/numa-reduce/numa-reduce.go
@@ -4,18 +4,48 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/numaproj/numaflow-go/pkg/reducer"
 	"github.com/redpanda-data/benthos/v4/public/service"
 )
 
 var numareduceConfigSpec = service.NewConfigSpec().
 	Summary("a Numaflow reduce input").Field(service.NewIntField("maxBatchSize").Default(10))
 
+var myInput *numaReduceInput
+
 type numaReduceInput struct {
-	maxBatchSize int
+	maxBatchSize    int
+	batchChannelIn  chan []byte
+	batchChannelOut chan []byte
+}
+
+func reduce(ctx context.Context, keys []string, inputCh <-chan reducer.Datum, md reducer.Metadata) reducer.Messages {
+	// sum up values for the same keys
+	intervalWindow := md.IntervalWindow()
+	_ = intervalWindow
+	var resultKeys = keys
+
+	myInput.batchChannelIn = make(chan []byte, myInput.maxBatchSize)
+	myInput.batchChannelOut = make(chan []byte, myInput.maxBatchSize)
+
+	for d := range inputCh {
+		myInput.batchChannelIn <- d.Value()
+	}
+	close(myInput.batchChannelIn)
+
+	messages := reducer.MessagesBuilder()
+	for d := range myInput.batchChannelOut {
+		messages = messages.Append(reducer.NewMessage(d).WithKeys(resultKeys))
+	}
+
+	return messages
 }
 
 func (g *numaReduceInput) Connect(ctx context.Context) error {
-	return nil
+	numaServer := reducer.NewServer(reducer.SimpleCreatorWithReduceFn(reduce))
+	err := numaServer.Start(context.Background())
+
+	return err
 }
 
 func (n *numaReduceInput) Close(ctx context.Context) error {
@@ -25,8 +55,30 @@ func (n *numaReduceInput) Close(ctx context.Context) error {
 func (n *numaReduceInput) ReadBatch(ctx context.Context) (service.MessageBatch, service.AckFunc, error) {
 
 	batch := make(service.MessageBatch, 0)
+	syncStore := make([]*service.SyncResponseStore, 0)
+
+	for m := range n.batchChannelIn {
+		msg, store := service.NewMessage(m).WithSyncResponseStore()
+		syncStore = append(syncStore, store)
+		batch = append(batch, msg)
+	}
 
 	return batch, func(ctx context.Context, err error) error {
+		for _, store := range syncStore {
+			msgs := store.Read()
+
+			for _, msg := range msgs {
+				for _, m := range msg {
+					data, err := m.AsBytes()
+
+					if err != nil {
+						n.batchChannelOut <- data
+					}
+
+				}
+			}
+		}
+		close(n.batchChannelOut)
 		return nil
 	}, nil
 }
@@ -40,10 +92,17 @@ func newNumaReduceInput(conf *service.ParsedConfig) (service.BatchInput, error) 
 		return nil, fmt.Errorf("maxBatchSize must be greater than 0, got: %v", maxBatchSize)
 	}
 
-	return service.InputBatchedWithMaxInFlight(maxBatchSize, &numaReduceInput{maxBatchSize}), nil
+	myInput = &numaReduceInput{
+		maxBatchSize,
+		make(chan []byte),
+		make(chan []byte),
+	}
+
+	return service.InputBatchedWithMaxInFlight(maxBatchSize, myInput), nil
 }
 
 func init() {
+
 	err := service.RegisterBatchInput("numareduce", numareduceConfigSpec,
 		func(conf *service.ParsedConfig, mgr *service.Resources) (service.BatchInput, error) {
 			return newNumaReduceInput(conf)
